a6433b65f2d841559212891f548b6841f4c7a62a
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2025-02-25 23:53:28 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+
+namespace NZWorksAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+            // Manually added swagger to see the endpoints
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                // Manually added swagger to see the endpoints
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
